4fdc88ffd015bf8f4f92238fc1cc761ea154cceb
alissonfgc <alissonfgc01@gmail.com>
2024-05-20 00:02:47 -0300

    Commiting new functions

--- portifolio_react_sass/src/App.jsx
+++ portifolio_react_sass/src/App.jsx
@@ -1,12 +1,14 @@
 import MainContent from './components/MainContent';
+import TopMenu from './components/TopMenu';
 
 import './styles/components/app.sass';
-import TopMenu from './components/TopMenu';
 
 function App() {
   return (
     <div id="portfolio">
-      <h1>Full-Stack Java Developer</h1>
+      <h2>Olá 👋 eu sou o Alisson</h2>
+      <h1>Full-Stack <br />Java Developer</h1>
+      <h2>Tenho (var) anos de idade <br /> Conheça meus projetos!</h2>
       <TopMenu />
       <MainContent />
     </div>
