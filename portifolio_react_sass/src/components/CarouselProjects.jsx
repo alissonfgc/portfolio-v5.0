69e8abc6eb797bf407ff1a5014015c14114c74e6
alissonfgc <alissonfgc01@gmail.com>
2024-05-29 04:16:50 -0300

    Fixing the slide

---
+++ portifolio_react_sass/src/components/CarouselProjects.jsx
@@ -0,0 +1,65 @@
+// import Carousel from "react-multi-carousel";
+// import "react-multi-carousel/lib/styles.css";
+
+// // import '../styles/components/carouselprojects.scss'
+
+// import Carousel from "react-multi-carousel";
+// import "react-multi-carousel/lib/styles.css";
+
+// const responsive = {
+//     desktop: {
+//         breakpoint: { max: 3000, min: 1024 },
+//         items: 3,
+//         slidesToSlide: 3 // optional, default to 1.
+//     },
+//     tablet: {
+//         breakpoint: { max: 1024, min: 464 },
+//         items: 2,
+//         slidesToSlide: 2 // optional, default to 1.
+//     },
+//     mobile: {
+//         breakpoint: { max: 464, min: 0 },
+//         items: 1,
+//         slidesToSlide: 1 // optional, default to 1.
+//     }
+// };
+
+
+// // const CarouselProjects = () => {
+// //     const responsive = {
+// //         superLargeDesktop: {
+// //             breakpoint: { max: 4000, min: 3000 },
+// //             items: 5
+// //         },
+// //         desktop: {
+// //             breakpoint: { max: 3000, min: 1024 },
+// //             items: 3
+// //         },
+// //         tablet: {
+// //             breakpoint: { max: 1024, min: 464 },
+// //             items: 2
+// //         },
+// //         mobile: {
+// //             breakpoint: { max: 464, min: 0 },
+// //             items: 1
+// //         }
+// //     };
+// //     return (
+
+// //         <Carousel responsive={responsive} autoPlay={true} infinite={true}>
+// //             <div>Item 1</div>
+// //             <div>Item 2</div>
+// //             <div>Item 3</div>
+// //             <div>Item 4</div>
+// //             <div>Item 5</div>
+// //             <div>Item 6</div>
+// //             <div>Item 7</div>
+// //             <div>Item 8</div>
+// //             <div>Item 9</div>
+// //         </Carousel>
+
+// //     )
+// // }
+
+// export default CarouselProjects
+
--- portifolio_react_sass/src/components/SecundaryProjects.jsx
+++ portifolio_react_sass/src/components/SecundaryProjects.jsx
@@ -1,9 +1,43 @@
 import '../styles/components/secundaryprojects.scss'
+//import CarouselProjects from './CarouselProjects'
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+    slidesToSlide: 3
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    slidesToSlide: 2
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1
+  }
+};
 
 const SecundaryProjects = () => {
   return (
     <section id='secundary-projects'>
+      <div id='secundary-projects-title'>
         <h2>Outros projetos</h2>
+      </div>
+      <div id='filter-for-projects'>
+        <button class='button-filter'>Java</button>
+        <button class='button-filter'>Java Spring</button>
+        <button class='button-filter'>PHP</button>
+        <button class='button-filter'>MySQL</button>
+        <button class='button-filter'>JavaScript</button>
+        <button class='button-filter'>CSS</button>
+        <button class='button-filter'>Bootstrap</button>
+        <button class='button-filter'>Tailwind</button>
+      </div>
+      <div id='carousel-secundary-projects'>
+      {/* Aqui vai ter um slider */}
+      </div>
     </section>
   )
 }
