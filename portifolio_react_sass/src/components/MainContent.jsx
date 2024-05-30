449a0fd5ffcd4be34b9bbea04db73f0e56ad0f35
alissonfgc <alissonfgc01@gmail.com>
2024-05-30 10:26:09 -0300

    Commiting new sections on branch front-end

--- portifolio_react_sass/src/components/MainContent.jsx
+++ portifolio_react_sass/src/components/MainContent.jsx
@@ -2,12 +2,14 @@ import MainProjects from './MainProjects'
 import SecundaryProjects from './SecundaryProjects'
 
 import '../styles/components/maincontent.sass'
+import AboutMeSection from './AboutMeSection'
 
 const MainContent = () => {
   return (
     <div id="main-content">
       <MainProjects />
       <SecundaryProjects />
+      <AboutMeSection />
     </div>
   )
 }
