a8e84df5a1d9f774eb227b3b42c9f824e0023714
alissonfgc <alissonfgc01@gmail.com>
2024-06-05 00:14:44 -0300

    Fixing spacements and portability of curriculum section

--- portifolio_react_sass/src/components/CurriculumSection.jsx
+++ portifolio_react_sass/src/components/CurriculumSection.jsx
@@ -13,27 +13,30 @@ const socialNetworks = [
 const CurriculumSection = () => {
     return (
         <sction id="curriculum-section">
-            <div id="curriculum-title">
-                <h2>Curriculo</h2>
-            </div>
-            <div id="curriculum-content">
+            <div id="section-content">
+                <div id="curriculum-title">
+                    <h2>Curriculo</h2>
+                </div>
+
                 <div id="div-curriculum-img">
                     <img id="curriculum-img" src={CurriculumImage} alt="Curriculum" />
                     <a id="link-button" href={PDFFile} target="_blank" >Visualizar curriculo <FaFilePdf /></a>
                 </div>
-                <div id="secundary-content">
-                    {socialNetworks.map((network) => (
-                        <a href={network.link} class="link-button" id={network.name} key={network.name}>
-                            {network.name} {network.icon}
-                        </a>
-                    ))}
-                    <a href={PDFFile} class="link-button" download="Curriculo-Alisson-Fernandes"> Download curriculo <FaFileDownload /></a>
+
+                <div id="secundary-content-spacement">
+                    <div id="secundary-content">
+                        {socialNetworks.map((network) => (
+                            <a href={network.link} class="link-button" id={network.name} key={network.name}>
+                                {network.name} {network.icon}
+                            </a>
+                        ))}
+                        <a href={PDFFile} class="link-button" download="Curriculo-Alisson-Fernandes"> Download curriculo <FaFileDownload /></a>
+                    </div>
                 </div>
+
             </div>
         </sction>
     )
 }
-
-// rel="noopener noreferrer"
 
 export default CurriculumSection
