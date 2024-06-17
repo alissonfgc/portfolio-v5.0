bd5eb646239aa08b9bd4cbc3aad49fc0ff24cace
alissonfgc <alissonfgc01@gmail.com>
2024-06-17 01:49:34 -0300

    Commiting the navabar menu responsive

--- portifolio_react_sass/src/components/TopMenu.jsx
+++ portifolio_react_sass/src/components/TopMenu.jsx
@@ -1,33 +1,32 @@
+import "../styles/components/topmenu.scss";
+
 import Logo from "../img/Logo.png";
 
-import "../styles/components/topmenu.sass";
-
 const TopMenu = () => {
-  return (
-    <nav id="top-menu">
-      <img src={Logo} alt="Alissonfgc" />
-      <div id='link-list'>
-        <ul>
-          <li>
-            <a href="#">Inicio</a>
-          </li>
-          <li>
-            <a href="#">Projetos</a>
-          </li>
-          <li>
-            <a href="#">Habilidades</a>
-          </li>
-          <li>
-            <a href="#">Curriculo</a>
-          </li>
-          <li>
-            <a href="#">Contato</a>
-          </li>
-        </ul>
-      </div>
-      <p>botao idioma</p>
-    </nav>
-  )
+    return (
+        <div id="nav-space">
+            <nav>
+                <input type="checkbox" id="sidebar-active"></input>
+                <label for="sidebar-active" class="open-sidebar-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="32" viewBox="0 -960 960 960" width="32"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" /></svg>
+                </label>
+                <label id="overlay" for="sidebar-active"></label>
+                <div class="links-container">
+                    <label for="sidebar-active" class="close-sidebar-button">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="32" viewBox="0 -960 960 960" width="32"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" /></svg>
+                    </label>
+
+                    <a class="home-link" href="">
+                        <img src={Logo} alt="Alissonfgc"></img>
+                    </a>
+                    <a href="">Projetos</a>
+                    <a href="">Habilidades</a>
+                    <a href="">Curriculo</a>
+                    <a href="">Contato</a>
+                </div>
+            </nav>
+        </div>
+    );
 }
 
 export default TopMenu
